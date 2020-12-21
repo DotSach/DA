3b201cb15a8eb5c94d269d36ada80bd588b76016
kieukhang185 <71631430+kieukhang185@users.noreply.github.com>
2020-12-21 09:25:22 +0700

    Add files via upload

---
+++ frmLongin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DA
+{
+    public partial class frmLongin : Form
+    {
+        public frmLongin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            if(txtUsername.Text == "kieukhang" && txtPass.Text == "1230")
+            {
+                //this.Hide();
+                
+                frmAdmin fma = new frmAdmin();
+                fma.id = 0;
+
+                fma.ShowDialog();
+                this.Close();
+                txtPass.Clear();
+                txtUsername.Clear();
+                txtUsername.Focus();
+                
+                
+            }else if(txtUsername.Text =="caoan" && txtPass.Text == "2535") 
+            {
+                //this.Hide();
+                
+                frmAdmin fma = new frmAdmin();
+                fma.id = 1;
+                
+                fma.ShowDialog();
+                this.Close();
+                txtPass.Clear();
+                txtUsername.Clear();
+                txtUsername.Focus();
+                
+            }
+            else
+                MessageBox.Show("Sai Thông Tin Tài Khoản Hoặc Mật Khẩu!", "Thông Báo", MessageBoxButtons.OKCancel);
+
+        }
+
+        private void btnCloseDN_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
